88e1a4472d2c8d2ff688fc3848f1542fddb849e1
masasixpack <81848199+masasixpack@users.noreply.github.com>
2022-04-22 12:01:30 +0900

    タスクの追加機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // liタグの子要素にdivタグを設定
+  li.appendChild(div);
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  // button(完了)タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  // button(削除)タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  // divタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
